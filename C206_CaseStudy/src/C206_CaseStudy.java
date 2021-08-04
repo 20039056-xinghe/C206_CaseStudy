8450b7b8ae003c978e1fa2c0e8186444fab29486
Marcus <20047045@myrp.edu.sg>
2021-08-04 08:36:31 +0800

    added an arrayList for registration, RegistrationMenu method, AddRegistration method, checkForDupeRegiNum method to check when adding to ensure unique registration ID.

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,70 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
+	private ArrayList<Registration> regList = new ArrayList<Registration>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+	private void RegisterMenu() { //Done by Marcus
+		System.out.println("1. Register for Tuition Timetable"); //Done by Marcus
+		System.out.println("2. View All Registration"); //Done by Marcus
+		System.out.println("3. Delete Registration"); //Done by Marcus
+	}
+	
+	private void AddRegistration() { //Done by Marcus
+		//registration number = Registration id must be unique.
+			//	tuition timetable id
+			//	student’s email
+			//	status which is initially set to “Pending”
+			//	registration date/time
+		int regiNum = readInt("Registration ID > "); //Done by Marcus
+		int timeTabelID = readInt("Time Table ID > "); //Done by Marcus
+		String stuEmail = readString("Student's Email"); //Done by Marcus
+		
+		
+		boolean repeat = checkForDupeRegiNum(regiNum);
+		
+		if(repeat == false) {  //Done by Marcus
+			regList.add(new Registration(regiNum, timeTabelID, stuEmail)); //Done by Marcus
+		}else { //Done by Marcus
+			System.out.println("Please use a unique Registration ID"); //Done by Marcus
+		}
 
 	}
 
+	private boolean checkForDupeRegiNum(int regiNum) { //Done by Marcus
+		boolean repeat = false;
+		for (Registration x : regList) {
+			if (x.getRegiNo() == regiNum) {
+				repeat = true;
+			}
+		}
+		return repeat;
+	}
+	
+	public static String readString(String prompt) { // Copied from Helper and Pasted by Marcus
+		System.out.print(prompt);
+		return new java.util.Scanner(System.in).nextLine();
+	}
+	
+	public static int readInt(String prompt) { //Copied from Helper and Pasted by Marcus
+		int input = 0; 
+		boolean valid = false;
+		while (!valid) {
+			try {
+				input = Integer.parseInt(readString(prompt));
+				valid = true;
+			} catch (NumberFormatException e) {
+				System.out.println("*** Please enter an integer ***");
+			}
+		}
+		return input;
+	}
+	
 }
---
+++ C206_CaseStudy/src/Registration.java
@@ -0,0 +1,61 @@
+import java.time.LocalDateTime;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20047045, 1 Aug 2021 3:44:22 am
+ */
+
+public class Registration {
+
+	//	registration number = Registration id must be unique.
+	//	tuition timetable id
+	//	student’s email
+	//	status which is initially set to “Pending”
+	//	registration date/time
+
+	private int regiNo;  //Done by Marcus
+	private int timeTableID; //Done by Marcus
+	private String email; //Done by Marcus
+	private String status; //Done by Marcus
+	private LocalDateTime regiDateTime; //Done by Marcus
+	
+	public Registration(int regiNo, int timeTableID, String email) { //Done by Marcus
+
+		this.regiNo = regiNo; //Done by Marcus
+		this.timeTableID = timeTableID; //Done by Marcus
+		this.email = email; //Done by Marcus
+		this.status = "Pending"; //Done by Marcus
+		this.regiDateTime = LocalDateTime.now(); //Done by Marcus
+	}
+
+	public int getRegiNo() { //Done by Marcus
+		return regiNo;
+	}
+
+	public int getTimeTableID() { //Done by Marcus
+		return timeTableID;
+	}
+
+	public String getEmail() { //Done by Marcus
+		return email;
+	}
+
+	public String getStatus() { //Done by Marcus
+		return status; 
+	}
+ 
+	public LocalDateTime getRegiDateTime() { //Done by Marcus
+		return regiDateTime;
+	}
+
+	public void setStatus(String status) { //Done by Marcus
+		this.status = status;
+	}
+	
+	
+	
+	
+}
