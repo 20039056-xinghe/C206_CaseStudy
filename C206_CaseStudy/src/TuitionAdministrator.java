d7476493c4c1c59b4614693744ad7962451e00fa
Marcus <20047045@myrp.edu.sg>
2021-08-05 15:06:17 +0800

    added comment for tuitionadministrator class

--- C206_CaseStudy/src/TuitionAdministrator.java
+++ C206_CaseStudy/src/TuitionAdministrator.java
@@ -6,21 +6,21 @@
  * 20047045, 5 Aug 2021 3:03:10 pm
  */
 
-public class TuitionAdministrator {
+public class TuitionAdministrator { //done by Marcus
 
 	private String username;
 	private String password;
 	
-	public TuitionAdministrator(String username, String password) {
+	public TuitionAdministrator(String username, String password) { //done by Marcus
 		this.username = username;
 		this.password = password;
 	}
-
-	public String getUsername() {
+ 
+	public String getUsername() { //done by Marcus
 		return username;
 	}
 
-	public String getPassword() {
+	public String getPassword() { //done by Marcus
 		return password;
 	}
 
