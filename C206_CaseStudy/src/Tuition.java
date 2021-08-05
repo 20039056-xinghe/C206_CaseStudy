84b66570cd10666013d652e5f3420fb01c11e9d2
YU XING HE <20039056@myrp.edu.sg>
2021-08-05 08:30:45 +0800

    Tuition class

---
+++ C206_CaseStudy/src/Tuition.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * YU XING HE, 5 Aug 2021 8:26:48 am
+ */
+
+public class Tuition {
+
+}
