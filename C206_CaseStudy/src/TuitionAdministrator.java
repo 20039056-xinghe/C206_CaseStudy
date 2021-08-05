7c9ed8abfcaedc2d882deba13c61e802562972de
Marcus <20047045@myrp.edu.sg>
2021-08-05 15:04:43 +0800

    added tutionAdministrator

---
+++ C206_CaseStudy/src/TuitionAdministrator.java
@@ -0,0 +1,28 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20047045, 5 Aug 2021 3:03:10 pm
+ */
+
+public class TuitionAdministrator {
+
+	private String username;
+	private String password;
+	
+	public TuitionAdministrator(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	
+}
