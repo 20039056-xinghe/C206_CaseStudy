0acad4b21be713b0f057f4149751175c01815134
20029895 <20029895@DESKTOP-DBSBS91>
2021-08-05 15:10:36 +0800

    Tuition manager class created - Jason

---
+++ C206_CaseStudy/src/TuitionManager.java
@@ -0,0 +1,19 @@
+
+public class TuitionManager {
+
+		
+		private String managerUsername;
+		private String managerPassword;
+		
+		public TuitionManager(String managerUsername, String managerPassword) {
+			this.managerUsername = managerUsername;
+			this.managerPassword = managerPassword;
+	}
+		public String getmanagerUsername() {
+			return managerUsername;
+		}
+		
+		public String getmanagerPassword() {
+			return managerPassword;
+		}
+}
